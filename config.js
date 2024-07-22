abdb0a7dae9f5ef1d77133ee7ad38e25b57759f9
ErlanggaLexy <121508815+ErlanggaLexy@users.noreply.github.com>
2024-07-22 13:51:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,9 +18,9 @@ module.exports = {
   nodes: [
     {
         name: "Node #1",
-        host: "four.lavalink.api.timelessnesses.me",
+        host: "lava-v4.ajieblogs.eu.org",
         port: 80,
-        password: "youshallnotpass",
+        password: "https://dsc.gg/ajidevserver",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: false
