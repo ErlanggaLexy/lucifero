1e8ff3d00097bc7bc0a01e1586b0cc9aae197ed0
ErlanggaLexy <121508815+ErlanggaLexy@users.noreply.github.com>
2024-07-22 14:17:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ module.exports = {
   commandsDir: './commands',  
   embedColor: "#db0032",
   musicardTheme:"themes15", //Goes from themes1 to themes19
-  activityName: "You", // This is bot status Write Anything here 
+  activityName: "YOU", // This is bot status Write Anything here 
   activityType: "WATCHING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
-  CheckmarkIcon: "https://cdn.discordapp.com/attachments/1230824451990622299/1230836684774576168/7762-verified-blue.gif",
+  CheckmarkIcon: "https://cdn.discordapp.com/attachments/1052112819006480426/1263549604566401106/maddie_creates-gif.gif?ex=669aa3c5&is=66995245&hm=f0458a7c8623f90718eb10f317e974dfa22f6f1057157fd34f49b09169cbc171&",
   MusicIcon:"https://media.discordapp.net/attachments/1230824451990622299/1236664581364125787/music-play.gif",
   embedTimeout: 5,  // Timeout before the button interaction embeds are deleted ( Default - 5 seconds)
   errorLog: "", 
@@ -18,9 +18,9 @@ module.exports = {
   nodes: [
     {
         name: "Node #1",
-        host: "lavalink-legacy.jompo.cloud",
-        port: 2333,
-        password: "jompo",
+        host: "lava-v3.ajieblogs.eu.org",
+        port: 80,
+        password: "https://dsc.gg/ajidevserver",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: false
